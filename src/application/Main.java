9aee4a444c25f2ed293ba871c843d2c548572e7c
Matheus Silvestre <matheussilvestre534@gmail.com>
2023-03-03 09:40:46 -0300

    SpecialMove - Promotion

--- src/application/Main.java
+++ src/application/Main.java
@@ -38,6 +38,12 @@ public class Main {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for Promotion (B/N/R/Q): ");
+					String type = read.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
